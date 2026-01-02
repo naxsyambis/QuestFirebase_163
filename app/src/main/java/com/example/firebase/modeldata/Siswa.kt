ba9323b62c1317e589bd6a727e7626ab0fba937c
naxsyambis <naxsyambis@gmail.com>
2026-01-02 13:34:48 +0700

    tambah model data Siswa dan fungsi pemetaan

---
+++ app/src/main/java/com/example/firebase/modeldata/Siswa.kt
@@ -0,0 +1,19 @@
+package com.example.firebase.modeldata
+
+data class Siswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+data class DetailSiswa(
+    val id: Long = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = ""
+)
+
+fun DetailSiswa.toDataSiswa(): Siswa = Siswa(id, nama, alamat, telpon)
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(id, nama, alamat, telpon)
+
+
