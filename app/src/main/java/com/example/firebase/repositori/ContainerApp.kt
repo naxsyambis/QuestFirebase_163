038264c22deaa65e83bb55626674e8f8c1b6c165
naxsyambis <naxsyambis@gmail.com>
2026-01-02 14:02:58 +0700

    Menyiapkan class AplikasiDataSiswa untuk mengelola dependensi aplikasi secara terpusat melalui DefaultContainerApp.

---
+++ app/src/main/java/com/example/firebase/repositori/ContainerApp.kt
@@ -0,0 +1,22 @@
+package com.example.firebase.repositori
+
+import android.app.Application
+import com.example.firebase.ui.theme.FirebaseTheme
+
+interface ContainerApp {
+    val repositorySiswa: RepositorySiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    override val repositorySiswa: RepositorySiswa by lazy {
+        FirebaseRepositorySiswa()
+    }
+}
+
+class AplikasiDataSiswa : Application() {
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        this.container = DefaultContainerApp()
+    }
+}
