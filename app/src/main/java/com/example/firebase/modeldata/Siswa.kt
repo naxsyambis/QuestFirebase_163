e03b91e575840f8a5713d31c9323c213946858f8
naxsyambis <naxsyambis@gmail.com>
2026-01-02 13:35:27 +0700

    Menambahkan UIStateSiswa untuk menangani status UI pada form siswa.

--- app/src/main/java/com/example/firebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/firebase/modeldata/Siswa.kt
@@ -17,3 +17,12 @@ fun DetailSiswa.toDataSiswa(): Siswa = Siswa(id, nama, alamat, telpon)
 fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(id, nama, alamat, telpon)
 
 
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
