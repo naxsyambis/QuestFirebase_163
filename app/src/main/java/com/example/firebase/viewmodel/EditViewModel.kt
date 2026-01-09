baaa17ccb7be07f091965b30466ddf782a689dbb
naxsyambis <naxsyambis@gmail.com>
2026-01-10 01:03:32 +0700

    membuat class editwiewmodel

---
+++ app/src/main/java/com/example/firebase/viewmodel/EditViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.firebase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.firebase.modeldata.DetailSiswa
+import com.example.firebase.modeldata.UIStateSiswa
+import com.example.firebase.modeldata.toUiStateSiswa
+import com.example.firebase.repositori.RepositorySiswa
+import com.example.firebase.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(value = UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+
+}
