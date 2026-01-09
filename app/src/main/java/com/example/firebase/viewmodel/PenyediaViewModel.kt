d0feb2f92a6a0743cb0bd854f96b4572ba141394
naxsyambis <naxsyambis@gmail.com>
2026-01-10 01:07:09 +0700

    update penyedia view model agar detail dan edit ke panggil

--- app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/firebase/viewmodel/PenyediaViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.firebase.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
@@ -15,5 +16,14 @@ object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiDataSiswa().container.repositorySiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container.repositorySiswa) }
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
+
+        initializer {
+            EditViewModel(this.createSavedStateHandle(), aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
     }
 }
