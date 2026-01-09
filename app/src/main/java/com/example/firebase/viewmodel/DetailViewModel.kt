fde14fd121d0ca68ea6d5ccb30e9bc14169703b5
naxsyambis <naxsyambis@gmail.com>
2026-01-10 00:54:01 +0700

    menambahkan sealed statusUIDetail

---
+++ app/src/main/java/com/example/firebase/viewmodel/DetailViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.firebase.viewmodel
+
+import com.example.firebase.modeldata.Siswa
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
